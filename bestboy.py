b2a57268287a1668bd543f3c5d408c19e0a6fa61
briner <briner@bestboy1>
2016-11-17 23:23:07 +0100

    added seems_an_important_movie

---
+++ bestboy.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import os
+import sys
+import time
+import subprocess
+import shlex
+import signal
+import datetime
+import re
+# pip
+from hurry.filesize import size, alternative
+#
+import bestboy_cfg
+
+MKV="output.mkv"
+RE_DURATION=re.compile("^DURATION\s+:\s+(\d{2}):(\d{2}):(\d{2})\.\d+$")
+
+
+def preexec_function():
+    os.setpgrp()
+
+class ConsoleLine:
+    def __init__(self):
+        self.len=0
+    def write(self, string):
+        sys.stdout.write(string)
+        sys.stdout.flush()
+        self.len+=len(string)
+    def erase(self):
+        if self.len>0:
+            sys.stdout.write("\b \b"*self.len)
+        sys.stdout.flush()
+        self.len=0
+    def newline(self):
+        if self.len:
+            sys.stdout.write(os.linesep)
+            sys.stdout.flush()
+        self.len=0
+console_line=ConsoleLine()
+
+def seems_a_important_movie(filepath):
+    lline=subprocess.check_output(shlex.split("/usr/bin/mediainfo "+filepath)).decode().splitlines()
+    duration_t=[0,0,0]
+    for line in lline:
+        match=RE_DURATION.search(line)
+        if match:
+            duration_t=[int(e) for e in RE_DURATION.search(line).groups()]
+            break
+    #
+    if datetime.timedelta(0,5,0) < datetime.timedelta(*duration_t):
+        return True
+    return False
+
+
+def main(cmd):
+    print("Launch it with cmd:")
+    print(cmd)
+
+    #
+    fh_log=open("ffmpeg.log","a")
+    process=subprocess.Popen(shlex.split(cmd),
+                             stdin=subprocess.PIPE,
+                             stdout=fh_log,
+                             stderr=fh_log,
+                             preexec_fn=preexec_function)
+
+    def signal_handler(asignal, frame):
+        console_line.newline()
+        print('You pressed Ctrl+C!')
+        stop_it=input("Do you want to stop this record [y/N]")
+        if stop_it.lower()=="y":
+            print(process.pid)
+            process.send_signal(signal.SIGINT)
+            sys.exit(0)
+        else:
+            print("keep it")
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    #
+    start=datetime.datetime.now()
+    state="not_started"
+    while True:
+        # TODO:    /dev/video1: Device or resource busy
+        if not os.path.isfile(MKV):
+            if state=="not_started":
+                print("file ({}) not yet created !".format(MKV))
+                state="started"
+            time.sleep(1)
+            continue
+        now=datetime.datetime.now()
+        now_str=now.strftime("%Hh%Mm%Ss")
+        delta_str=".".join(str(now-start).split(".")[:-1])
+        size_si_str=size(os.path.getsize(MKV), system=alternative)
+        now_str=datetime.datetime.now().strftime("%Hh%Mm%Ss")
+        msg="@{} size : {:>6}, duration: {}".format(now_str, size_si_str, delta_str)
+        console_line.erase()
+        console_line.write(msg)
+        time.sleep(1)
+
+if "__main__" == __name__:
+
+    config=bestboy_cfg.config[bestboy_cfg.default_configname]
+    main(config.cmd)
---
+++ bestboy_cfg.py
@@ -0,0 +1,14 @@
+class BestboyConfig():
+    cmd='''ffmpeg -thread_queue_size 512 -f v4l2 -input_format yuyv422 -i /dev/video1
+                  -thread_queue_size 512 -f v4l2 -input_format yuyv422 -i /dev/video0
+                  -thread_queue_size 512 -f pulse -ac 2 -i default
+                  -map 0 -map 1 -map 2
+                  -c:v:0 h264
+                  -c:v:1 h264
+                  -c:a:2 mp3
+                  -y
+                  {filname}.mkv'''
+
+default_configname="default"
+default_filename="test"
+config={"default": BestboyConfig()}
